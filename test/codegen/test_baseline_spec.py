e8d1616fe32bdcf92908f92b1f8e651d6b3283aa
Alex Knight <alex@alexknight.co.uk>
2021-11-04 00:46:58 +0000

    MInor tweak

--- test/codegen/test_baseline_spec.py
+++ test/codegen/test_baseline_spec.py
@@ -9,14 +9,15 @@ def test_default_output():
     test_prog = createTree("""""")
 
     target.visit(test_prog)
-    expected_value = textwrap.dedent(".data\n"
-                                     ".text\n"
-                                     ".global main\n"
-                                     "main:\n"
-                                     "push %rbp\n"
-                                     "movq %rsp, %rbp\n"
-                                     "pop %rbp\n"
-                                     "ret\n")
+    expected_value = (
+        ".data\n"
+         ".text\n"
+         ".global main\n"
+         "main:\n"
+         "push %rbp\n"
+         "movq %rsp, %rbp\n"
+         "pop %rbp\n"
+         "ret\n")
 
 
     assert target.data + target.body == expected_value
